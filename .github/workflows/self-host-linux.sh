dcdb20509134bc979fa7a19d8c9420f52ad20f1b
David Liu <david.yx.liu@oracle.com>
2021-04-14 07:09:25 -0400

    Create self-host-linux.sh

---
+++ .github/workflows/self-host-linux.sh
@@ -0,0 +1,15 @@
+set -e
+githubUrl=$1
+token=$2
+
+# Create a folder
+mkdir actions-runner && cd actions-runner
+# Download the latest runner package
+curl -o actions-runner-linux-x64-2.277.1.tar.gz -L https://github.com/actions/runner/releases/download/v2.277.1/actions-runner-linux-x64-2.277.1.tar.gz
+# Extract the installer
+tar xzf ./actions-runner-linux-x64-2.277.1.tar.gz
+
+# Create the runner and start the configuration experience
+./config.sh --url $1 --token $token
+# Last step, run it!
+./run.sh
